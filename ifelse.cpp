01dc3ab09cf8019acd1fd626b5b734a41ca15ecd
Avni-garg22 <avnisandeep2002@gmail.com>
2024-08-29 10:22:08 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,81 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout<<"enter two numbers";
+//     cin>>a>>b;
+
+//     if(a>b)
+//     cout<<"a is the greatest";
+//     else
+//     cout<<"b is the greatest";
+
+// return 0;
+// }  
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cout<<"enter the number";
+//     cin>>a;
+
+//     if(a>0)
+//     cout<<"no is positive";
+//     if(a<0)
+//     cout<<"no is negative";
+//     else
+//     cout<<"zero";
+
+// return 0;
+// }  
+
+
+
+// ---wap to print whether ch is smaller or uppercase or numeric ?--
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char ch;
+//     cout<<"enter a character";
+//     cin>>ch;
+
+//     if(ch>='a' && ch<='z')
+//     cout<<"this is lowercase";
+//     else if (ch>='A' && ch<='Z')
+//     cout<<"this is uppercase";
+//     else if (ch>='0' && ch<='9')
+//     cout<<"this is numeric number";
+//     else
+//     cout<<"invalid";
+
+// return 0;
+// }  
+
+
+
+//wap to print the given condition checking its ascii value//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int c;
+    cout<<"enter one value";
+    cin>>c;
+
+    if(c>='a' && c<='z')
+    cout<<"this is lowercase";
+    else if (c>='A' && c<='Z')
+    cout<<"this is uppercase";
+    else if (c>='0' && c<='9')
+    cout<<"this is numeric number";
+    else
+    cout<<"invalid";
+
+return 0;
+}  
